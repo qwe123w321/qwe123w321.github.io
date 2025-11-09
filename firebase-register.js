639abde0ed968e580d3556fdba9c5c4a2b84a9be
qwe123w321 <59343771+qwe123w321@users.noreply.github.com>
2025-11-09 19:31:29 +0800

    Update firebase-register.js

--- firebase-register.js
+++ firebase-register.js
@@ -250,7 +250,7 @@ function ensureGlobalFunctions() {
             
             // 如果是第3步，顯示提交按鈕
             const submitButton = document.querySelector('.btn-submit');
-            if (submitButton && nextStep === 3) {
+            if (submitButton && nextStep === 4) {
                 submitButton.style.display = 'block';
             }
             
